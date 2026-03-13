149c50aa9326fddbf82705b05b70a09dbb359e74
Nate Buckareff <nate.buckareff@gmail.com>
2026-03-12 18:56:10 -0700

    rm everything

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./constants.js";
-export * from "./free-surface.js";
-export * from "./grid.js";
-export * from "./lbm-core.js";
-export * from "./math.js";
--- src/math.js
+++
@@ -1,7 +0,0 @@
-export function clamp(value, min, max) {
-  return Math.max(min, Math.min(max, value));
-}
-
-export function finiteOr(value, fallback = 0) {
-  return Number.isFinite(value) ? value : fallback;
-}
